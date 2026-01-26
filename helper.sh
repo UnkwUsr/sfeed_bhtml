f494483916fcf435dacf2f7b37403aa05798906f
UnkwUsr <ktoto2707043@gmail.com>
2026-01-26 20:34:30 +0300

    update script

--- helper.sh
+++ helper.sh
@@ -7,13 +7,14 @@ cd ~/.sfeed/ || exit 1
 sfeed_update
 (cd ~/Projects/sfeed_html_full/ && cargo build)
 
-mkdir -p prev_html
-new_res="./prev_html/res_$(date +%s).html"
-~/Projects/sfeed_html_full/target/debug/a ./feeds/* >> "$new_res"
+mkdir -p generated_htmls
+new_res="./generated_htmls/res_$(date +'%Y-%m-%d_%H:%M:%S').html"
+cat ./feeds/* | ~/Projects/sfeed_html_full/target/debug/a >> "$new_res"
 
-# if res is not empty
+# if file is not empty
 if [ -s "$new_res" ]; then
     quteb "$(realpath "$new_res")"
 else
+    # remove redundant empty file
     rm "$new_res"
 fi
